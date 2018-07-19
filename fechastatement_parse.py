62e363b805b9e53683e4331579bd1592074811de
Carlos Piad <carlospiad@gmail.com>
2018-07-19 17:08:47 -0500

    open excel ,crete csv and select correct cells

--- fechastatement_parse.py
+++ fechastatement_parse.py
@@ -1,14 +1,35 @@
 import xlrd
+import csv
 
 book = xlrd.open_workbook("data/data.xls")
-print("The number of worksheets is {0}".format(book.nsheets))
-print("Worksheet name(s): {0}".format(book.sheet_names()))
+
 sh = book.sheet_by_index(0)
-print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
-print("Cell D30 is {0}".format(sh.cell_value(rowx=20, colx=3)))
-for rx in range(sh.nrows):
-    print(sh.row(rx))
-#
+
+
+with open('data/output.csv', 'w+') as csvout:
+    writer = csv.writer(csvout, lineterminator='\n')
+#     next(cs, None)  # skip the first row from the reader, the old header
+    writer.writerow(['Date', 'Payee','Conepto','Referencia', 'Outflow', 'Inflow']) # write new header
+    for rx in range(12,23):
+        writer.writerow((
+            sh.cell_value(rx, colx=1),
+            sh.cell_value(rx, colx=6),
+            sh.cell_value(rx, colx=11),
+            sh.cell_value(rx, colx=16),
+            sh.cell_value(rx, colx=18),
+            sh.cell_value(rx, colx=23)
+            ))
+
+
+# print("The number of worksheets is {0}".format(book.nsheets))
+# print("Worksheet name(s): {0}".format(book.sheet_names()))
+# print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
+# print("Cell D30 is {0}".format(sh.cell_value(rowx=20, colx=3)))
+# for rx in range(sh.nrows):
+#     print(sh.row(rx))
+
+# for rx in range(12,23):
+#     print(sh.row(rx))
 # with open('data/MovimientosFecha.txt', newline='',encoding='latin-1') as csvfile:
 #   with open('data/bgynab.csv', 'w+') as csvout:
 #     writer = csv.writer(csvout, lineterminator='\n')
